b6654e70763aa136816329621bb34117af8780a6
Diogo Trindade <diogoguarda@netcabo.pt>
2015-10-28 20:36:01 +0000

    1 linha

--- Projecto/src/rcom.c
+++ Projecto/src/rcom.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
 {
 	(void) signal(SIGALRM, alarmHandler);
 	if(strcmp(argv[1], "1") == 0)
-		initAppLayer("/dev/ttyS0", TRANSMITTER, 38400, 20000, 3, 3, "pinguim.gif");
+		initAppLayer("/dev/ttyS4", TRANSMITTER, 38400, 20000, 3, 3, "pinguim.gif");
 	else
 		initAppLayer("/dev/ttyS0", RECEIVER, 38400, 20000, 3, 3, "pinguim.gif");
 	return 0;
